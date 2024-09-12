ddc912f88ea4224428186cd58a5a35e3e3217dfa
Gulshan Kumar <inbox.gulshan@outlook.com>
2024-09-12 23:10:49 +0530

    Added Comments for the Building Blocks interfaces to clearly explain the purpose

--- src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -1,10 +1,19 @@
 using MediatR;
 namespace BuildingBlocks.CQRS;
 
-public interface ICommandhandler<in TCommand> : ICommandHandler<TCommand, Unit> where TCommand : ICommand<Unit>
+/// <summary>
+/// Interface for a command handler that doesn't return a value.
+/// </summary>
+/// <typeparam name="TCommand">The type of the command.</typeparam>
+public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit> where TCommand : ICommand<Unit>
 {
 }
 
+/// <summary>
+/// Interface for a command handler that returns a value.
+/// </summary>
+/// <typeparam name="TCommand">The type of the command.</typeparam>
+/// <typeparam name="TResponse">The type of the response.</typeparam>
 public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse> where TCommand : ICommand<TResponse> where TResponse : notnull
 {
 }
--- src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -2,6 +2,13 @@ using MediatR;
 
 namespace BuildingBlocks.CQRS;
 
-public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse> where TQuery : IQuery<TResponse> where TResponse : notnull
+/// <summary>
+/// Interface for a query handler that returns a value.
+/// </summary>
+/// <typeparam name="TQuery">The type of the query.</typeparam>
+/// <typeparam name="TResponse">The type of the response.</typeparam>
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+    where TResponse : notnull
 {
 }
