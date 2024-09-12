d1383bed7351a520e48d2fec6b07cce75a54e67e
Gulshan Kumar <inbox.gulshan@outlook.com>
2024-09-12 23:13:31 +0530

    Refactored The CreateProductHandler to  make use of common Command and Query interfaces.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,39 @@
+using BuildingBlocks.CQRS;
+using Catalo.API.Models;
 using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductCommand(string Name, string Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResponse>;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
 
-public record CreateProductResponse(Guid Id);
+public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResponse>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    /// <summary>
+    /// Handles the creation of a new product.
+    /// </summary>
+    /// <param name="command">The command containing the product details.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the product creation.</returns>
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         // Add the business logic here
-        throw new NotImplementedException();
+        // Create Product entity from command object
+        // Save to database
+        // return CreateProductResult result with Id
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // Save to Database
+
+        return new CreateProductResult(product.Id);
     }
 }
