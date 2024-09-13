42622a93196c46a081123bcc9d37fab0ed5c279b
Gulshan Kumar <inbox.gulshan@outlook.com>
2024-09-13 09:30:00 +0530

    Added Create Product Enpoint

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the container
+builder.Services.AddCarter(null);
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP Request pipeline
+app.MapCarter();
 
 app.Run();
